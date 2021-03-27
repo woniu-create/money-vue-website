df3c0fc16d67cab6191a2974abae62b2a75ae30c
luyandan <2103029766@qq.com>
2021-03-27 15:17:07 +0800

    deploy

---
+++ precache-manifest.836a36046ab3345c2b88c887b5ac70c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ed44d29bb7a73f0bc5",
+    "url": "/money-vue-website/css/app.0468649a.css"
+  },
+  {
+    "revision": "ba489aca9db7adb11ad69458264802dd",
+    "url": "/money-vue-website/index.html"
+  },
+  {
+    "revision": "c9ed44d29bb7a73f0bc5",
+    "url": "/money-vue-website/js/app.9bbc4bf3.js"
+  },
+  {
+    "revision": "5c726dc38d4b4e6ff35b",
+    "url": "/money-vue-website/js/chunk-vendors.00ee8dbb.js"
+  },
+  {
+    "revision": "2389b9051ff3dfa899176100d61983f5",
+    "url": "/money-vue-website/manifest.json"
+  },
+  {
+    "revision": "b67ded2083a46bfd91b9ee68428e12cd",
+    "url": "/money-vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-vue-website/robots.txt"
+  }
+]);
